9c79d51eba6ec2c7a8018b8ba82c73e0c7981955
slavanr45 <slava.r45@gmail.com>
2023-06-28 23:31:09 +0700

    fix gendif.py (step 2)

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,8 +3,10 @@ import argparse
 
 parser = argparse.ArgumentParser(
   description='Compares two configuration files and shows a difference.')
-parser.add_argument('first_file')
-parser.add_argument('second_file')
+parser.add_argument('first_file', type=str)
+parser.add_argument('second_file', type=str)
+parser.add_argument('-f', '--format', type=str,
+                    default='plain', help='set format of output')
 args = parser.parse_args()
 
 
